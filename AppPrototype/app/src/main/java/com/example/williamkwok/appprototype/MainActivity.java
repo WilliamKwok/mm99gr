f45482399973d8e3ce22b52c52a548e8af408ecb
William Kwok <williamkwok92@gmail.com>
2015-07-02 15:52:16 -0600

    image and launch icon added.

--- AppPrototype/app/src/main/java/com/example/williamkwok/appprototype/MainActivity.java
+++ AppPrototype/app/src/main/java/com/example/williamkwok/appprototype/MainActivity.java
@@ -149,7 +149,7 @@ public class MainActivity extends Activity implements OnClickListener{
             try{
                 Message message = new MimeMessage (session);
                 message.setFrom(new InternetAddress("testing12345testt@gmail.com"));
-                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("zzazengna@gmail.com"));
+                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("williamkwok92@gmail.com"));
                 message.setSubject(textMessage);
                 message.setContent(textMessage, "text/html; charset=utf-8");
                 Transport.send(message);
